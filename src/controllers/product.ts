426096aba0059e8247f0c78310eabd820fd4b8cb
thealitahir <bsef13m049@gmail.com>
2021-09-01 04:10:43 +0500

    major functionality achieved

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,5 +1,5 @@
 import { ProductModel, Product} from '../models/product'
-import { Category } from '../models/category'
+import { Category, CategoryModel } from '../models/category'
 
 export class ProductController {
 
@@ -21,8 +21,40 @@ export class ProductController {
     getDiscount({name, user_id, invoice_amount}: any) {
         return Product.findOne({where:{name:name},
             include: [{model:Category}]
-        }).then((product: any)=>{
-            console.log("fetched product", product.dataValues.category);
+        }).then(async (product: any)=>{
+            
+            const prod = product.dataValues
+            const category = prod.category.dataValues
+            let discount = -1;
+            if((!prod.discount || prod.discount < 1) && category) {
+                if((!category.discount || category.discount < 1) && category.categoryId){ 
+                    const finalCategory: any = await Category.findByPk(category.categoryId, {
+                        attributes: ProductController.productAttributes
+                    });
+                    if(!finalCategory.discount || finalCategory.discount < 1){
+                        discount = -1
+                    } else {
+                        discount = finalCategory.discount
+                    }
+
+                } else {
+                    discount = category.discount
+                }
+
+            }
+            else {
+                discount = prod.discount
+                
+            }
+            if(discount > 1){
+                return calculateDiscount(discount,invoice_amount)+`%`
+            } else {
+                return discount;
+            }
         });
     }
+}
+
+const calculateDiscount = (discount : number, invoice: number) => {
+    return discount/(invoice/100)
 }
